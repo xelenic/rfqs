2d99b8facd597799a0a6982b48893b2d864ee338
sanjayaharshana <sanjayaharasana@gmail.com>
2026-09-13 21:47:00 +0530

    Example Testings

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,7 @@
 <?php
 
-test('the application returns a successful response', function () {
+test('the root url redirects into the admin panel', function () {
     $response = $this->get('/');
 
-    $response->assertStatus(200);
+    $response->assertRedirect('/admin');
 });
